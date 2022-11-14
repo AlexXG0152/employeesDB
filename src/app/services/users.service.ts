b6c2a83b463da71260011f8e757547a66273b0ff
Alex <al.gr.0017@gmail.com>
2022-11-14 19:18:49 +0300

    feat: create homepage and search

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, share } from 'rxjs';
+import { Employees } from '../interfaces/employees';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<Employees[]> {
+    return this.http.get<Employees[]>('https://dummyjson.com/users');
+  }
+  getUserByName(name: string): Observable<Employees> {
+    return this.http.get<Employees>(
+      `https://dummyjson.com/users/search?q=${name}`
+    );
+  }
+}
