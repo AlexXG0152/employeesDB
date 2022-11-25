121d57b3b73be0ce195cbe2a03b56c0a421b6542
Alex <al.gr.0017@gmail.com>
2022-11-25 15:28:54 +0300

    feat: mockups for employees education and family members

--- server/src/app/config/uploadToMongoDB.js
+++ server/src/app/config/uploadToMongoDB.js
@@ -1,15 +1,16 @@
 import { MongoClient } from "mongodb";
 import fs from 'fs'
+require("dotenv").config({ path: "./src/app/environments/.env" });
 
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb+srv://Admin:CQnx7ySdomAIxl35@cluster0.ickh3tv.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB;
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     const database = client.db("test");
-    const foods = database.collection("employees");
+    const foods = database.collection("employeesEducations"); // COLLECTION!
 
     // create an array of documents to insert
     // const docs = [
@@ -18,7 +19,7 @@ async function run() {
     //   { name: "donut", healthy: false },
     // ];
     let docs = fs.readFileSync(
-        "../../../../client/src/assets/empls.json",
+        "../../../../client/src/assets/emplsEducation.json", // FILENAME!
         "utf8");
     docs = JSON.parse(docs);
     // this option prevents additional documents from being inserted if one fails
