b7f49a3ccf0331a5b26980c3f0ec45d6c4503588
Alex <al.gr.0017@gmail.com>
2022-12-01 14:13:25 +0300

    feat: add todayBirthdays backend

---
+++ server/src/app/controllers/reports.controller.js
@@ -0,0 +1,41 @@
+// import { MongoClient } from "mongodb";
+
+// const agg = [
+//   {
+//     $match: {
+//       birthDate: {
+//         $regex: new RegExp("^01.12."),
+//       },
+//     },
+//   },
+// ];
+
+// const client = await MongoClient.connect("", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const coll = client.db("").collection("");
+// const cursor = coll.aggregate(agg);
+// const result = await cursor.toArray();
+// await client.close();
+
+const asyncHandler = require("express-async-handler");
+const Employee = require("../models/employee.model");
+
+exports.todayBirthdays = asyncHandler(async (req, res) => {
+  const agg = [
+    {
+      $match: {
+        birthDate: {
+          $regex: new RegExp(req.body.date),
+        },
+      },
+    },
+  ];
+  try {
+    const todayBirthdays = await Employee.aggregate(agg);
+    res.json(todayBirthdays.length);
+  } catch (error) {
+    return res.status(200).send({ error });
+  }
+});
